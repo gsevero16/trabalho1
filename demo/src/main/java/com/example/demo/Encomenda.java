fa43169a0a88797bfe2e66f1f124af0ed738b000
Gabriel Severo <gsevero16@gmail.com>
2023-10-08 22:30:28 -0300

    Created variables

--- demo/src/main/java/com/example/demo/Encomenda.java
+++ demo/src/main/java/com/example/demo/Encomenda.java
@@ -1,5 +1,8 @@
 package com.example.demo;
 
 public class Encomenda {
-    
+    int id;
+    int enderecoOrigem;
+    int enderecoDestino;
+    int peso;
 }
--- demo/src/main/java/com/example/demo/Orcamento.java
+++ demo/src/main/java/com/example/demo/Orcamento.java
@@ -1,5 +1,16 @@
 package com.example.demo;
 
+import java.util.Date;
+
 public class Orcamento {
-    
+    int id;
+    int custoBasico;
+    int adicionalPeso;
+    int enderecoOrigem;
+    int enderecoDestino;
+    int peso;
+    int desconto;
+    int imposto;
+    int custoFinal;
+    Date dataSimulacao;
 }
