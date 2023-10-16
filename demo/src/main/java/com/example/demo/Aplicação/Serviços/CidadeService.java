49b2e2fb9e0f98104b944625efe0cb368a1140ee
Gabriel Severo <gsevero16@gmail.com>
2023-10-15 23:10:42 -0300

    feito cidade

--- demo/src/main/java/com/example/demo/Aplicação/Serviços/CidadeService.java
+++ demo/src/main/java/com/example/demo/Aplicação/Serviços/CidadeService.java
@@ -1,11 +1,20 @@
 package com.example.demo.Aplicação.Serviços;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
 public class CidadeService {
 
     public CidadeService(){
     }
 
     public boolean verificaCidade(int cep){
-        return true;
+        List<Integer> cepList = new ArrayList<>(Arrays.asList(25000, 26000, 27000, 28000, 29000, 30000, 31000, 32000, 33000, 34000));
+        
+        return cepList.contains(cep);
     }
 }
--- demo/src/main/java/com/example/demo/Aplicação/VerificaCidade.java
+++ demo/src/main/java/com/example/demo/Aplicação/VerificaCidade.java
@@ -1,5 +1,20 @@
 package com.example.demo.Aplicação;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.example.demo.Aplicação.Serviços.CidadeService;
+
+@Component
 public class VerificaCidade {
-    
+    private CidadeService cs;
+
+    @Autowired
+    public VerificaCidade(CidadeService cs){
+        this.cs = cs;
+    }
+
+    public boolean consultaCep(int cep){
+        return this.cs.verificaCidade(cep);
+    }
 }
